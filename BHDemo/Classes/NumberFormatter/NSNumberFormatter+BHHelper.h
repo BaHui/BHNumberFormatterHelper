bbb2f962cc10a6ee9090e09201fbced2abca065a
QiaoBaHui <qiaobahuiyouxiang@163.com>
2018-12-02 14:09:28 +0800

    初始化项目

---
+++ BHDemo/Classes/NumberFormatter/NSNumberFormatter+BHHelper.h
@@ -0,0 +1,17 @@
+//
+//  NSNumberFormatter+BHHelper.h
+//  BHDemo
+//
+//  Created by QBH on 2018/12/2.
+//  Copyright © 2018 QiaoBaHui. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSNumberFormatter (BHHelper)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BHDemo/Classes/NumberFormatter/NSNumberFormatter+BHHelper.m
@@ -0,0 +1,13 @@
+//
+//  NSNumberFormatter+BHHelper.m
+//  BHDemo
+//
+//  Created by QBH on 2018/12/2.
+//  Copyright © 2018 QiaoBaHui. All rights reserved.
+//
+
+#import "NSNumberFormatter+BHHelper.h"
+
+@implementation NSNumberFormatter (BHHelper)
+
+@end
